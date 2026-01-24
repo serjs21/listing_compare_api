373cf105837a9323a60b172b95c7d45b333099fb
OmniToaster <omnitoaster@OmniToasters-MacBook-Pro.local>
2026-01-24 17:42:54 +0700

    Initial commit

---
+++ src/main/java/org/example/listing_compare_api/ListingCompareApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.listing_compare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingCompareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingCompareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/listing_compare_api/ListingCompareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.listing_compare_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingCompareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
